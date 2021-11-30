40019362de2791c7f3d2a16d1d910976e8e26271
beykun <bayurasukmaraga@gmail.com>
2021-11-30 16:24:31 +0700

    initial commit

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,34 @@
+<template>
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item" v-for="(item, index) in list" v-bind:key="index">
+      <span class="active" v-if="isLast(index)">{{ showName(item) }}</span>
+      <nuxt-link :to="item" v-else>{{ showName(item) }}</nuxt-link>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  },
+  methods: {
+    isLast (index) {
+      return index === this.list.length - 1
+    },
+    showName (item) {
+      if (item.meta && item.meta.label) {
+        item = item.meta && item.meta.label
+      }
+      if (item.name) {
+        item = item.name
+      }
+      return item
+    }
+  }
+}
+</script>
